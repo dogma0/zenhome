5bc925b2f8d85219ffbdd2965b032850f19ba20a
flai <franklai@protonmail.com>
2018-10-11 23:28:14 -0400

    Signin form's UI works properll

--- client/src/components/AuthForm.tsx
+++ client/src/components/AuthForm.tsx
@@ -1,18 +1,13 @@
 import * as React from 'react'
 import { Form, Icon, Input, Modal, Tabs } from 'antd';
 
-const AuthInputForm = (props: any) => {
-    const FormItem = Form.Item;
-    const { isLogin, form } = props;
-    const { getFieldDecorator } = form;
-    return <Form className="signup-form">
-        {isLogin && <FormItem>
-            {getFieldDecorator('name', {
-                rules: [{ required: true, message: 'Please input your name!' }],
-            })(
-                <Input prefix={<Icon type="smile" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Name" />
-            )}
-        </FormItem>}
+const FormItem = Form.Item;
+
+const getFormAndFieldDecor = (p) => ({ form: p.form, getFieldDecorator: p.form.getFieldDecorator})
+
+const AuthInputForm = (props) => {
+    const { getFieldDecorator } = getFormAndFieldDecor(props)
+    return (<Form className="signup-form">
         <FormItem>
             {getFieldDecorator('email', {
                 rules: [{ required: true, message: 'Please input your email!' }],
@@ -27,10 +22,35 @@ const AuthInputForm = (props: any) => {
                 <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
             )}
         </FormItem>
-        {isLogin && <FormItem>
-            <a className="login-form-forgot" href="">Forgot password</a>
-        </FormItem>}
-    </Form>
+    </Form>)
+}
+
+const SignupForm = (props) => {
+    const { form, getFieldDecorator } = getFormAndFieldDecor(props)
+    return (
+        <div>
+            <FormItem>
+                {getFieldDecorator('name', {
+                    rules: [{ required: true, message: 'Please input your name!' }],
+                })(
+                    <Input prefix={<Icon type="smile" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Name" />
+                )}
+            </FormItem>
+            <AuthInputForm form={form} />
+        </div>
+    )
+}
+
+const LoginForm = (props) => {
+    const { form } = getFormAndFieldDecor(props)
+    return (
+        <div>
+            <AuthInputForm form={form} />
+            <FormItem>
+                <a className="login-form-forgot" href="">Forgot password</a>
+            </FormItem>
+        </div>
+    )
 }
 
 interface IAuthForm {
@@ -41,36 +61,36 @@ interface IAuthForm {
     mutation: any,
     form: any
 }
-
 class AuthForm extends React.Component<IAuthForm> {
+    state = { isLoginForm: true }
     render() {
         const { visible, onCancel, onAuth, confirmLoading, mutation, form } = this.props;
         const TabPane = Tabs.TabPane
+        let { isLoginForm } = this.state
+        const titleText: string = isLoginForm ? "Login" : "Signup"
+        console.log(typeof(form))
         return (
             <Modal
                 visible={visible}
-                title="Signin"
-                okText="Signin"
+                title={titleText}
+                okText={titleText}
                 onCancel={onCancel}
                 onOk={onAuth(mutation)}
                 confirmLoading={confirmLoading}
             >
-                <Tabs defaultActiveKey="1">
+                <Tabs
+                    defaultActiveKey="1"
+                    onChange={() => { this.setState({ isLoginForm: !isLoginForm }) }}>
                     <TabPane tab="Login" key="1">
-                        <AuthInputForm
-                            isLogin={true}
-                            form={form}
-                        />
+                        <LoginForm form={form} />
                     </TabPane>
                     <TabPane tab="Signup" key="2">
-                        <AuthInputForm
-                            isLogin={false}
-                            form={form}
-                        />
+                        <SignupForm form={form} />
                     </TabPane>
                 </Tabs>
             </Modal>
         );
     }
 }
+
 export default Form.create()(AuthForm);
