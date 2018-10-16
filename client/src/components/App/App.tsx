0eb73b8c3e702a08646139001f8a70baa431900a
flai <franklai@protonmail.com>
2018-10-16 16:01:23 -0400

    Clean up playaround code

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,29 +1,14 @@
 import * as React from 'react';
 import Landing from '../Landing'
 import Header from '../Heading'
-// import gql from 'graphql-tag'
-import { Route, Link, Switch} from 'react-router-dom'
-import { Switch as AntSwitch} from 'antd';
 
 // const ME_QUERY = gql`
-//     query MeQuery {
-//         me {
-//             email
-//         }
-//     }
-// `
-
 class App extends React.Component {
     render() {
         return (
             <div>
                 <Header />
                 {/* <Route exact path="/" render={() => {
-                    return ((localStorage.getItem('token')) ?
-                        <Redirect to="/app" /> :
-                        <Landing />)
-                }} /> */}
-                <Switch>
                     <Route exact path="/" render={()=> <Landing/>}/>
                     <Route exact path="/app" render={()=> <p>APP PAGE</p>}/>
                 </Switch>
