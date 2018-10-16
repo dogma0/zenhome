14e64b485e462b012d9c1e8ec1e4585490135a94
furrysolver <frankjr.lai@gmail.com>
2018-10-16 16:06:50 -0400

    Basic CRUB on Offer and Touring models

--- server/src/resolvers/Query.ts
+++ server/src/resolvers/Query.ts
@@ -3,5 +3,13 @@ import { getUserId } from '../utils'
 export const Query = {
   me: (_parent, _args, ctx) => {
     return ctx.db.user({ id: getUserId(ctx) })
-  }
+  },
+  offers: (_parent, _args, ctx) => {
+    return ctx.db.user(
+      { id: getUserId(ctx) }).offers()
+  },
+  tourings: (_parent, _args, ctx) => {
+    return ctx.db.user(
+      { id: getUserId(ctx) }).tourings()
+  },
 }
