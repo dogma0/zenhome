2195bd18cb7e4773accd2f9a84ab865dd1393177
flai <franklai@protonmail.com>
2018-10-22 13:50:41 -0400

    Removed exposure to the creator field in type Offer and Touring

--- server/src/resolvers/index.ts
+++ server/src/resolvers/index.ts
@@ -2,14 +2,10 @@ import { Query } from './Query'
 import { Mutation } from './Mutation'
 import { AuthPayload } from './AuthPayload'
 import { User } from './User'
-import { Offer } from './Offer'
-import { Touring } from './Touring'
 
 export const resolvers = {
   Query,
   Mutation,
   AuthPayload,
   User,
-  Offer,
-  Touring,
 }
